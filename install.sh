8c5ff64ebecda7393e731e88151ccf591ad2f3d0
yutaszk <yutaszk23@gmail.com>
2017-06-19 15:19:44 +0900

    Rebuild install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-if [ "Darwin" == 'Darwin' ]; then
+if [ "$(uname)" == 'Darwin' ]; then
 
 if !(type brew) &>/dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -15,7 +15,7 @@ elif (type apt)&>/dev/null; then
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y build-essential curl git redis-tools mysql-client
+sudo apt install -y bison build-essential curl git redis-tools mysql-client
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -52,33 +52,33 @@ if [ "$(uname)" == 'Darwin' ]; then
 elif type apt &>/dev/null; then
   wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
   sudo apt-get update
-  sudo apt-get install esl-erlang
-  sudo apt-get install elixir
+  sudo apt-get install -y esl-erlang
+  sudo apt-get install -y elixir
 fi
 
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source ~/.gvm/scripts/gvm
 gvm install go1.4 --binary
 gvm use go1.4
-gvm install `cat versions/golang`
-gvm use `cat versions/golang` --default
+gvm install `cat lib/versions/golang`
+gvm use `cat lib/versions/golang` --default
 
 curl -sSL https://get.haskellstack.org/ | sh
 
 git clone https://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
-nvm install `cat versions/node`
-nvm alias default `cat vertsions/node`
+nvm install `cat lib/versions/node`
+nvm alias default `cat lib/versions/node`
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 pyenv install miniconda3-latest
-conda create -n python2 python=`cat versions/python2`
-conda create -n python3 python=`cat versions/python3`
+conda create -n python2 python=`cat lib/versions/python2`
+conda create -n python3 python=`cat lib/versions/python3`
 pyenv global miniconda3-latest/envs/python3 miniconda3-latest/envs/python2
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install `cat versions/ruby`
+rbenv install `cat lib/versions/ruby`
 
 if [ "$(uname)" == 'Darwin' ]; then
   brew update
