d046f10ea59a7c58d41ae2aab8c088231110dd0f
yutaszk <yutaszk23@gmail.com>
2017-08-20 01:16:29 +0900

    Fix python install script

--- lib/python.sh
+++ lib/python.sh
@@ -1,6 +1,8 @@
 git -C ~/.pyenv pull || git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 export PATH=~/.pyenv/bin:$PATH
+eval "$(pyenv init -)"
 pyenv install miniconda3-latest
+pyenv global miniconda3-latest
 conda create -n python2 python=$(cat lib/versions/python2)
 conda create -n python3 python=$(cat lib/versions/python3)
 pyenv global miniconda3-latest/envs/python3 miniconda3-latest/envs/python2
