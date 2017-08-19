242e26e1d6e17337c538737a39740bd1f15055ff
yutaszk <yutaszk23@gmail.com>
2017-08-19 23:27:28 +0900

    Add rbenv global

--- install.sh
+++ install.sh
@@ -81,6 +81,7 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 export PATH=~/.rbenv/bin:$PATH
 rbenv install $(cat lib/versions/ruby)
+rbenv global $(cat lib/versions/ruby)
 
 if [ "$(uname)" == 'Darwin' ]; then
   brew update
--- lib/ruby.sh
+++ lib/ruby.sh
@@ -2,4 +2,5 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 export PATH=~/.rbenv/bin:$PATH
 rbenv install $(cat lib/versions/ruby)
+rbenv global $(cat lib/versions/ruby)
 
