676fb9d43bad36a9c2601a744bc79644988dd564
euxn23 <euxn23@gmail.com>
2018-03-23 17:55:08 +0900

    Fix apt default install

--- lib/apt.sh
+++ lib/apt.sh
@@ -1,7 +1,8 @@
+sudo apt install apt-add-repository
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y bison build-essential curl git redis-tools mysql-client zsh
+sudo apt install -y bison build-essential curl git redis-tools libssl-dev libreadline-dev zlib1g-dev mysql-client zsh
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
