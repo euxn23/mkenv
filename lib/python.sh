2c79cdde340e082fe8507baa192f48ac5b7d623e
euxn23 <euxn23@gmail.com>
2018-04-23 17:23:34 +0900

    Add pyenv global setting

--- lib/python.sh
+++ lib/python.sh
@@ -1,4 +1,5 @@
 git -C ~/.pyenv pull || git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 export PATH=~/.pyenv/bin:$PATH
 pyenv install $(cat lib/versions/python3)
+pyenv global $(cat lib/versions/python3)
 
